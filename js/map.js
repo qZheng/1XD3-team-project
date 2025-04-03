35b362ec093e0d224fa53ae7e36ff62bf0d83ba0
latte7 <lucas.zhxng@gmail.com>
2025-04-03 13:07:02 -0400

    Added click functionality to maps

--- js/map.js
+++ js/map.js
@@ -1,23 +1,40 @@
-// Initial map load
 window.onload = function() {
-    updateMap('McMaster+University,Hamilton+ON');
+    updateMap('Caroline+Place+Retirement+Residence,+Hamilton,+ON');
+    
+    document.querySelectorAll('.tile').forEach(tile => {
+        tile.addEventListener('click', function() {
+            const locationName = this.querySelector('h2').textContent;
+            
+            switch(locationName) {
+                case 'Caroline Place':
+                    updateMap('Caroline+Place+Retirement+Residence,+Hamilton,+ON');
+                    break;
+                case 'Community Living Hamilton':
+                    updateMap('Community+Living+Hamilton,+Hamilton,+ON');
+                    break;
+                case 'Shalom Village':
+                    updateMap('Shalom+Village,+Hamilton,+ON');
+                    break;
+                default:
+                    updateMap('McMaster+University,Hamilton+ON');
+            }
+        });
+    });
 };
 
 function updateMap(location) {
-    const apiKey = 'xxx'
+    const apiKey = 'AIzaSyBL-DlFXUL4sxfoWCNdHkYnvoARe70k7Zo';
     const mapHtml = `
-        <div>
-            <iframe
-                width="600"
-                height="450"
-                style="border:0"
-                loading="lazy"
-                allowfullscreen
-                referrerpolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${location}">
-            </iframe>
-        </div>
+        <iframe
+            width="600"
+            height="450"
+            style="border:0"
+            loading="lazy"
+            allowfullscreen
+            referrerpolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${location}">
+        </iframe>
     `;
     
-    document.getElementById('mapContainer').innerHTML = mapHtml;
+    document.getElementById('map').innerHTML = mapHtml;
 }
